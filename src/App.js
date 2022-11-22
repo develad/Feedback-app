c2b1f387dcea6c9004c6faa9f5b59e4fa8e247f3
develad <develadb@gmail.com>
2022-11-22 18:05:29 +0200

    adding to the Header component default Props and PropTypes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header />
-      <h1>My App</h1>
+      <Header
+      // bgColor="lime" textColor="white"
+      />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
     </>
   );
 }
