933f5e1df8afb0a91b7ee797d3ab2515dbbac3d8
develad <develadb@gmail.com>
2022-11-22 17:45:53 +0200

    adding to the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import PropTypes from "prop-types";
 
 function Header({ text }) {
-  return <div>{text}</div>;
+  return (
+    <header>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
 }
 
 Header.defaultProps = {
