9076189a9df62e1c09871cc59ee8c8dc85e00420
develad <develadb@gmail.com>
2022-11-22 20:46:49 +0200

    adding FeedbackList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import Feedbackitem from "./components/Feedbackitem";
 import Header from "./components/Header";
+import { feedbackData } from "./data/feedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+  const [feedback, setFeedback] = useState(feedbackData);
+
   return (
     <>
       <Header
       // bgColor="lime" textColor="white"
       />
       <div className="container">
-        <Feedbackitem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function Feedbackitem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState("This is an example of a feedback item");
+function Feedbackitem({ item }) {
+  //   const [rating, setRating] = useState(7);
+  //   const [text, setText] = useState("This is an example of a feedback item");
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   );
 }
